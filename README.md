88b814e4b37a15fdafde661b68259842912d52d1
Michael Stewart <mpstewartpgh@gmail.com>
2020-02-01 01:30:55 -0500

    initial commit
    - add README.md
    - add keybindings.js

---
+++ README.md
@@ -0,0 +1,5 @@
+# melvor-idle
+`melvor-idle` is a collection of userscripts I've authored for the idle game [Melvor Idle](https://melvoridle.com). 
+
+## Compatibility
+All scripts in this repository are meant to be run with Tampermonkey. Simply add them as usescripts. I don't think these will work with Greasemonkey.
---
+++ keybindings.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name         stewbeef's keybinds
+// @namespace    https://www.github.com/mpstewart
+// @version      0.1
+// @description  Keybinds for melvor idle
+// @author       stewbeef
+// @match        *melvoridle.com
+// @run-at       document-idle
+// @grant        none
+// ==/UserScript==
+
+// function alias map; in case function names change
+goToPage    = changePage
+var curPage = currentPage
+
+(function() {
+    'use strict';
+
+    const BANKPAGE = 2;
+
+    var cachePage;
+
+    document.addEventListener('keypress', function(e) {
+        switch(e.code) {
+            case 'Backquote':
+                toggleBankTab();
+                break;
+        }
+    });
+
+    function toggleBankTab() {
+        if (curPage == BANKPAGE) {
+            var nextPage = cachePage != 2 ? cachePage : BANKPAGE;
+            goToPage(nextPage);
+        }
+
+        else {
+            cachePage = curPage;
+            goToPage(BANKPAGE);
+        }
+    }
+
+    console.log("keybinds initialized...");
+})();
